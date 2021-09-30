71443b953784532fdb74c32cd4ef6c732c5efbe7
IT19387536-Crishani <87943150+IT19387536@users.noreply.github.com>
2021-09-30 22:04:20 +0530

    Add new Examination details form

--- frontend/src/components/User/ExamRegistration/AddExamRegistration.jsx
+++ frontend/src/components/User/ExamRegistration/AddExamRegistration.jsx
@@ -64,21 +64,23 @@ export default function ExamDetails() {
     <div>
           <Header/>
           <br/>
-           <h2 style={{display: 'flex',  justifyContent:'center', alignItems:'center',height:"60px"}}>Register for Semester examination</h2>
+           <h2 style={{display: 'flex',  justifyContent:'center', alignItems:'center',height:"60px"}}><b>Register for Examination</b></h2>
                 <div className="col-sm-6 p-3 bg-light" className="center" style={{marginLeft:'30%',marginRight:'30%'}}>
                   <div class="mb-3">
-                  <label class="form-label">Registration No. <sup className="text-danger" style={{fontSize:'10px'}}>*Required</sup></label>
+                  <label style={{color:'black'}} class="form-label"><b>Registration No. </b><sup className="text-danger" style={{fontSize:'10px'}}>*Required</sup></label>
                   <div className="input-group">
+                  <span className="input-group-text bg-dark"><i className="bi bi-card-heading text-warning"></i></span>
                     <input type="regNo" class="form-control"  placeholder="Enter Registration No." onChange={(e) =>{
                               setregNo(e.target.value);
                             }}/>
                             </div>
                   </div>
                   <div className="mb-3">
-                            <label className="form-label">Lesson Name <sup className="text-danger" style={{fontSize:'10px'}}>{Required}</sup></label>
+                            <label style={{color:'black'}} className="form-label"><b>Lesson Name </b><sup className="text-danger" style={{fontSize:'10px'}}>{Required}</sup></label>                           
                             <select class="form-select" aria-label="Default select example"  onChange={(e) =>{
                                       setlesson(e.target.value);
                                     }}>
+                                     
                             <option value="">Select a lesson</option>
                             <option value="IT & Software">IT & Software</option>
                             <option value="Development">Development</option>                         
@@ -90,15 +92,16 @@ export default function ExamDetails() {
                           
                           </div>
                   <div class="mb-3">
-                  <label className="form-label">Exam Date <sup className="text-danger" style={{fontSize:'10px'}}>{Required}</sup></label>
+                  <label style={{color:'black'}} className="form-label"><b>Exam Date </b><sup className="text-danger" style={{fontSize:'10px'}}>{Required}</sup></label>
                     <div className="input-group">
+                    <span className="input-group-text bg-dark"><i className="bi bi-calendar-check text-warning"></i></span>
                     <input type="date" class="form-control" onChange={(e) =>{
                               setexamDate(e.target.value);
                             }}/>
                             </div>
                   </div>
                   <div className="mb-3">
-                            <label className="form-label">Exam Session <sup className="text-danger" style={{fontSize:'10px'}}>{Required}</sup></label>
+                            <label style={{color:'black'}} className="form-label"><b>Exam Session </b><sup className="text-danger" style={{fontSize:'10px'}}>{Required}</sup></label>
                             <select class="form-select" aria-label="Default select example"  onChange={(e) =>{
                                       setsession(e.target.value);
                                     }}>
@@ -109,14 +112,15 @@ export default function ExamDetails() {
                           
                           </div>
                   <div class="mb-3">
-                    <label class="form-label">Amout <sup className="text-danger" style={{fontSize:'10px'}}></sup></label>
+                    <label style={{color:'black'}} class="form-label"><b>Amount </b><sup className="text-danger" style={{fontSize:'10px'}}></sup></label>
                     <div className="input-group">
+                    <span className="input-group-text bg-dark"><i className="bi bi-cash-coin text-warning"></i></span>
                     <input type="number" class="form-control"  placeholder="Enter Amount" onChange={(e) =>{
                               setamount(e.target.value);
                             }}/>
                             </div>
                   </div>
-                  <div className="col-md-14 col-sm-12" style={{ maxWidth: '70rem', margin: 'auto', padding: '10px' }}>
+                  <div style={{color:'black'}} className="col-md-14 col-sm-12" style={{ maxWidth: '70rem', margin: 'auto', padding: '10px' }}>
                 <div style={{ height: '300px' }}>
                     <div className="row row-cols-1 row-cols-md-3 g-4" className="text-center container">
                         <div className="col">
