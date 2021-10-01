cdfeaec2f61aa2793183d711eb412347fe78d811
IT19387536-Crishani <87943150+IT19387536@users.noreply.github.com>
2021-10-01 13:47:01 +0530

    Update ExamRegistration.js

--- backend/routes/ExamRegistration.js
+++ backend/routes/ExamRegistration.js
@@ -1,34 +1,64 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const router = require('express').Router();
+let ExamRegistration = require('../models/ExamRegistration');
 
-const ExamRegistration = new Schema({
-    regNo:{
-        type:String,
-        required:true
-    },
-    lesson:{
-        type:String,
-        required:true
-    },
-    examDate:{
-        type:Date,
-        required:true
-    },
-    session:{
-        type:String,
-        required:true
-    },
-    amount:{
-        type:String,
-        required:true
-    },
-    paymentStatus:{
-        type:String,
-        required:true
-    },
-},{
-    timestamps:true
+router.route('/add').post((req,res) => {
+    const regNo = req.body.regNo;
+    const lesson = req.body.lesson;
+    const examDate = req.body.examDate;
+    const session = req.body.session;
+    const amount = req.body.amount;
+    const paymentStatus = req.body.paymentStatus;
+    
+ 
+
+    const newExamRegistration = new ExamRegistration({
+        regNo,
+        lesson,
+        examDate,
+        session,
+        amount,
+        paymentStatus
+    });
+
+    newExamRegistration.save()
+        .then(() => res.json("Congargulations!! You are successfully Registered"))
+        .catch(err => res.status(400).json('Error : ' +err));
 });
 
-const Exam_Registration_Schema = mongoose.model('Exam_Registration_Schema',ExamRegistration);
-module.exports = Exam_Registration_Schema;
+router.route('/view').get((req,res) => {
+    ExamRegistration.find()
+        .then(ExamRegistration => res.json(ExamRegistration))
+        .catch(err => res.status(400).json('Error: ' +err)); 
+});
+
+router.route('/delete/:id').delete(async (req,res) => {
+    let id = req.params.id;
+    await ExamRegistration.findByIdAndDelete(id).then(() => {
+        res.status(200).send({status: "Successfully Unenroll from the lesson."});
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send({status: "Error while deleting lesson.", error: err.message});
+    }); 
+});
+
+router.route('/update/:id').post(async (req,res) => {
+    let id = req.params.id;
+    const amount = req.body.amount;
+    const lesson = req.body.lesson;
+    const examDate = req.body.examDate;
+    const session = req.body.session;
+    const updateExamRegistration={
+        amount,
+        lesson,
+        examDate,
+        session
+    }
+    const update = await ExamRegistration.findOneAndUpdate({_id:id},updateExamRegistration).then(() => {
+        res.status(200).send({status: "Successfully updated lesson details"});
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send({status: "Error while updating lesson details.", error: err.message});
+    });
+});
+
+module.exports = router;
